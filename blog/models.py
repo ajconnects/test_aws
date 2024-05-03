5d66735abfa71fcaa403f0698d02354c122926e9
Aj connect <ajiriiviero2021@gmail.com>
2024-05-03 14:29:34 +0200

    add

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField(max_length=2000)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from models import Post
 
 # Create your views here.
+def home(request):
+    data = Post.objects.all()
+    return render(request, 'blog/index.html', {'posts: data'})
